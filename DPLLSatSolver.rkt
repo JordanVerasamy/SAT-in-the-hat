524224ea7bcf3423a4e667a195a4dc5b9982722a
Jordan Verasamy <j.verasamy@gmail.com>
2014-12-08 12:04:42 -0500

    beginning to write logic functions

--- DPLLSatSolver.rkt
+++ DPLLSatSolver.rkt
@@ -1,10 +1,8 @@
 #lang racket
 
-
-;; a Literal is one of:
-;; -true
-;; -false
-;; -integer with 1+ representing variables and -1- representing negated variables
+;; a Literal is an:
+;; integer x with 1+ representing variables and -1- representing negated variables
+;; which is true iff (contains? assignment (list x true))
 
 ;; a Clause is a:
 ;; (listof Literal)
@@ -15,8 +13,40 @@
 ;; which is true iff all of its Clauses are true.
 
 ;; an Assignment is a:
-;; (listof (list int boolean))
-;; where the int is a variable and the bool is the value we've assigned to it
+;; (list int)
+;; where the int is a variable that is true (so 4 means variable 4 is true, whereas -2 means variable 2 is false)
+
+(define (contains? value list)
+  (cond
+    [(empty? list)
+     false]
+    [(equal? (first list) value)
+     true]
+    [else
+     (contains? value (rest list))]))
+
+(define (no-unassigned-variables? assignment CNF)
+  (cond
+    [(empty? CNF)
+     true]
+    [(empty? (first CNF))
+     (no-unassigned-variables? assignment (rest CNF))]
+    [(or (contains? (first (first CNF)) assignment) (contains? (* -1 (first (first CNF))) assignment))
+     (no-unassigned-variables? assignment (append (list (rest (first CNF))) (rest CNF)))]
+    [else
+     false]))
+
+(define (clause-is-true? assignment clause)
+  (cond
+    [(empty? clause)
+     false]
+    [(contains? (first clause) assignment)
+     true]
+    [else
+     (clause-is-true? assignment (rest clause))]))
+
+(define (CNF-is-true? assignment CNF)
+  (equal? CNF (filter (lambda (x) (clause-is-true? assignment x)) CNF)))
 
 (define (solve-SAT assignment CNF)
   (cond
@@ -25,11 +55,17 @@
      (CNF-is-true? assignment CNF)]
     ;;Pure Literal Elimination
     [(not (equal? false (get-pure-literal CNF)))
-     (solve-SAT (append assignment (list (get-pure-literal CNF) true)) CNF)]
+     (solve-SAT (append assignment (get-pure-literal CNF)) CNF)]
     ;;Unit Propagation
     [(not (equal? false (get-unit-literal CNF)))
-     (solve-SAT (append assignment (list (get-unit-literal CNF) true)) CNF)]
+     (solve-SAT (append assignment (get-unit-literal CNF)) CNF)]
     ;;Backtrack
     [else
-     (or (solve-SAT (append assignment (list (get-unknown-literal CNF) true)) CNF)
-         (solve-SAT (append assignment (list (get-unknown-literal CNF) false)) CNF))]))
+     (or (solve-SAT (append assignment (get-unknown-literal CNF)) CNF)
+         (solve-SAT (append assignment (* -1 (get-unknown-literal CNF))) CNF))]))
+     
+
+
+
+
+
